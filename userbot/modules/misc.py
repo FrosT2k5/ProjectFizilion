76ec01db1cbe5168dbc158ef8f936aa9b6218ee0
FrosT2k5 <androygaming@gmail.com>
2020-11-23 11:28:43 +0530

    Fixing repo url

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -94,7 +94,7 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "[Click here](https://github.com/ElytrA8/ProjectFizilion) to open Fizilion's GitHub Repo."
+        "[Click here](https://github.com/FrosT2k5/ProjectFizilion) to open Fizilion's GitHub Repo."
     )
 
 
