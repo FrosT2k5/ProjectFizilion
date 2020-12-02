65b355d73095708ab0a8078844c01c4ad1495136
FrosT2k5 <androygaming@gmail.com>
2020-12-03 01:03:23 +0530

    misc.py: Adding Support group

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -97,7 +97,10 @@ async def repo_is_here(wannasee):
         "[Click here](https://github.com/FrosT2k5/ProjectFizilion) to open Fizilion's GitHub Repo."
     )
 
-
+@register(outgoing=True, pattern="^.support$")
+async def grup(sapot):
+    await sapot.edit("Support Group: @ProjectFizilion")
+    
 @register(outgoing=True, pattern="^.raw$")
 async def raw(rawtext):
     the_real_message = None
@@ -133,7 +136,7 @@ CMD_HELP.update(
 ".repo"
 "\nUsage: If you are curious what makes the userbot work, this is what you need.\n\n"
 ".readme"
-"\nUsage: Provide links to setup the userbot and it's modules.\n\n"
+"\nUsage: Provide links to setup the userbot and it's modules\nAnd .support for support group\n\n"
 ".repeat <no.> <text>"
 "\nUsage: Repeats the text for a number of times. Don't confuse this with spam tho.\n\n"
 ".restart"
