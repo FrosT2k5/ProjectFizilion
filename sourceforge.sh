ea89edf943ad9bd94676b9f661a092789443558f
FrosT2k5 <androygaming@gmail.com>
2020-12-16 16:25:44 +0530

    sourceforge.sh: No need to specify /Fizilion/ anymore
    If u enter a file with just Filename like Downloads/file.zip then the code will add /Fizilion/ at the starting all by itself. And if u did put /Fizilion then it will just pass that value without adding another /Fizilion

--- sourceforge.sh
+++ sourceforge.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 FILE=${1?Error: what to put}
-
-expect -c " 
+TEST=${FILE:0:10}
+if [ $TEST == "/Fizilion/" ]; then
+    MFILE=$FILE
+else
+    MFILE="/Fizilion/"
+    MFILE+="${FILE}"
+fi
+expect -c "
 spawn sftp $SFUSER@frs.sourceforge.net
-expect \"yes/no\"
-send \"yes\r\"
-expect \"Password\"        
+#expect \"yes/no\"
+#send \"yes\r\"
+expect \"Password:\"
 send \"$SFPASS\r\"
 expect \"sftp> \"
 send \"cd $SFDIR\r\"
 set timeout -1
-send \"put $FILE\r\"
+send \"put $MFILE\r\"
 expect \"Uploading\"
 expect \"100%\"
 expect \"sftp>\"
